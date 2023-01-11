2e7f22e4cf94b5b6dbad8d49af90fb33e29ccb46
kooya3 <elyeestatua@gmail.com>
2023-01-11 17:50:33 +0300

    eslint debugged and no errors

--- personal-portfolio/pages/components/assets/server/webpack-runtime.js
+++ personal-portfolio/pages/components/assets/server/webpack-runtime.js
@@ -3,11 +3,16 @@
  * This devtool is neither made for production nor for readable output files.
  * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
  * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
+  .
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  *
-let browser = {module}
-/******/ (() => { // webpackBootstrap
+/******/ 
+
+module.exports = {
+  mode: 'production',
+};
+
+(() => { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({});
 /************************************************************************/
